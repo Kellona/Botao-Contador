98409018789be4ef058406e26e3e084213c88c99
Kellona Christina <105223748+Kellona@users.noreply.github.com>
2026-03-30 10:21:56 -0300

    Inicial

--- app.tsx
+++
@@ -1,9 +0,0 @@
-import { TrainingCounter } from "./components/TrainingCounter";
-
-export default function App() {
-  return (
-    <div className="size-full flex items-center justify-center p-4 bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
-      <TrainingCounter />
-    </div>
-  );
-}
